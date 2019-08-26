e8a3beafd2c33d2da7bfe3368850a52ea5184728
lipeflorentino <lipeflorentino2@gmail.com>
2019-08-26 19:29:58 -0300

    updating project

---
+++ api/crawler.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.crawl = async event => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
