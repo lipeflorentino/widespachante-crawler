6f3b0359ac0897f4dfb10a7682eebfff129a5f23
Ubuntu <ubuntu@ip-172-31-42-129.ec2.internal>
2019-08-27 23:54:42 +0000

    adicionando headers na response

--- api/crawler.js
+++ api/crawler.js
@@ -1,8 +1,11 @@
 'use strict';
 
-module.exports.crawl = async event => {
+module.exports.crawl = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers:{
+      'Access-Control-Allow-Origin':'*'
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
@@ -10,4 +13,5 @@ module.exports.crawl = async event => {
   };
 
   callback(null, response);
+
 };
